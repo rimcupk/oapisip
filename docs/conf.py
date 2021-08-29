9284d2ff9e0f7cfe9a2e8f12e44f6bb5a0cb5078
five76 <fiv_e76@mail.ru>
2021-08-29 13:43:19 +0500

    add images

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,13 @@ release = '1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.mathjax']
+extensions = ['sphinx.ext.mathjax',
+        'IPython.sphinxext.ipython_directive',
+        'IPython.sphinxext.ipython_console_highlighting',
+        'sphinx.ext.autodoc',
+        'sphinx.ext.doctest',
+        'sphinx.ext.inheritance_diagram',
+        ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
